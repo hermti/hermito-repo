da0064c9f9f6cbe30835220a0a6896630e812bd4
hermti <46742504+hermti@users.noreply.github.com>
2019-03-27 10:47:41 +0000

    up

--- zips/_integrieren/VerzeichnisseErstellen.ps1
+++ zips/_integrieren/VerzeichnisseErstellen.ps1
@@ -9,15 +9,25 @@ function Unzip
 }
 
 $zips=gci $outpath -filter *.zip |Sort-Object name -Descending
+$anzahl=$zips.count
+$zaehler=1
+write-host "Entpacken..." -ForegroundColor green
 foreach ($zipfile in $zips)
 {
+    write-host "$zaehler / $anzahl" -ForegroundColor green
+    $zaehler++
     $Zipdatei=$zipfile.fullname
     Unzip $Zipdatei $outpath
 }
 
+write-host "Bereinigen..." -ForegroundColor green
 $ordnerliste=gci $outpath -Directory
+$anzahl=$ordnerliste.count
+$zaehler=1
 foreach ($ordner in $ordnerliste)
 {
+    write-host "$zaehler / $anzahl" -ForegroundColor green
+    $zaehler++
     $Ordnerpfad = $ordner.fullname
     $Ordnername = $ordner.name
     write-host $Ordnerpfad
